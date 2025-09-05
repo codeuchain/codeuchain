185527307dcbcfee0efa672bea60cc0c4f1432d9
Joshua Wink <Joshf.wink@gmail.com>
2025-09-04 21:39:19 -0500

    feat: Bump Python package to v1.0.0 with type hints support

--- packages/python/setup.py
+++ packages/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codeuchain",
-    version="0.1.0",
+    version="1.0.0",
     description="Agape-optimized Python implementation of CodeUChain",
     author="CodeUChain Team",
     packages=find_packages(),
